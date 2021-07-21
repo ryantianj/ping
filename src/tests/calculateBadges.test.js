b5baf6c5c7ff7a8380a9cb3e9fe417b3d038ca24
marcus <haojie.chua@gmail.com>
2021-07-21 23:19:08 +0800

    reorg files for tests

--- src/tests/calculateBadges.test.js
+++ src/tests/calculateBadges.test.js
@@ -1,14 +1,9 @@
-import { findAllBadges } from './calculateBadges';
-import store from './store';
+import React from 'react';
 import { render, fireEvent } from '@testing-library/jest-native';
 
-// Backend
+import { findAllBadges } from '../calculateBadges';
+
 test('runs findAllBadges successfully', async () => {
     const j = await findAllBadges();
     expect(j).toBeGreaterThanOrEqual(104);
-});
-
-test('is user defined?', () => {
-    const user = store.getState().user;
-    expect(user).toBeDefined();
 });
---
+++ src/tests/renderScreens.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/jest-native';
+
+import Login from '../screens/LoginScreen';
+
+// test('renders login screen', () => {
+//     const { getAllByText, getByPlaceholderText } = render(<Login />);
+//     expect(getAllByText('Email').length).toBe(2);
+// });
